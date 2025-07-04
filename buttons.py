805f3c41850ae2e7c178a6d7ae72e53e738213a4
MatTerrezza <72928801+MatTerrezza@users.noreply.github.com>
2025-07-04 19:48:13 +0300

    buttons.py

--- buttons.py
+++ buttons.py
@@ -6,10 +6,9 @@ def get_main_keyboard():
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     btn_me = types.KeyboardButton("About me")
     btn_usl = types.KeyboardButton("Услуги")
-    btn_aut = types.KeyboardButton("Автоматизация бизнес")
-    btn_serv = types.KeyboardButton("FAQ")
+    btn_serv = types.KeyboardButton("Связаться со мной")
     
-    keyboard.add(btn_aut, btn_usl)    
+    keyboard.add(btn_usl)    
     keyboard.add(btn_me, btn_serv)
     return keyboard
 
@@ -20,27 +19,35 @@ def get_pdf_button():
 
 def get_automatic_keyboard():
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
-    btn_office = types.KeyboardButton("🖥Цифровой офис под ключ")
+    btn_office = types.KeyboardButton("🖥Цифровая экосистема под ключ")
     btn_crm = types.KeyboardButton("🗂CRM")
-    btn_tgbot = types.KeyboardButton("🤖Telegram бот")    
-    btn_eco = types.KeyboardButton("Создание цифровой экосистемы")
-    btn_mapp = types.KeyboardButton("🌐Telegram Mini app")
+    btn_mapp = types.KeyboardButton("🌐Telegram bot & Mini app")
     btn_integr = types.KeyboardButton("🔃Интеграции")
-    btn_back = types.KeyboardButton("◀️Назад")
+    btn_back = types.KeyboardButton("🔺Назад")
     
-    keyboard.add(btn_office, btn_crm, btn_tgbot, btn_mapp, btn_integr, btn_eco, btn_back)
+    keyboard.add(btn_office, btn_crm, btn_mapp, btn_integr, btn_back)
     return keyboard
     
 def get_services_keyboard():
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
-    btn_cons = types.KeyboardButton("Консультации")
+    btn_cons = types.KeyboardButton("Consulting")
+    btn_aut = types.KeyboardButton("Автоматизация бизнеса")
+  
+    btn_back = types.KeyboardButton("🔺Назад")
+    
+    keyboard.add(btn_cons, btn_aut)
+    keyboard.add(btn_back)
+    return keyboard
+
+def get_consulting_keyboard():
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+    btn_con = types.KeyboardButton("Консультации")
     btn_audit = types.KeyboardButton("Глубинный аудит")
     btn_stsess = types.KeyboardButton("Стратегические сессии")
-    btn_wow = types.KeyboardButton("WOW сервис")
-
-    btn_back = types.KeyboardButton("◀️Назад")
+    btn_wow = types.KeyboardButton("WOW-сервис")
+    btn_back = types.KeyboardButton("🔺Назад")    
     
-    keyboard.add(btn_cons, btn_audit)
+    keyboard.add(btn_con, btn_audit)
     keyboard.add(btn_stsess, btn_wow)
     keyboard.add(btn_back)
     return keyboard
